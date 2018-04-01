aaabcdd3e461f7ed7e1980aff9749dccc8ed92d2
Vidyadhar <justvidyadhar@gmail.com>
2018-04-01 13:03:03 +0530

    Add login views and templates

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def home(request):
 	return render(request, "core/home.html")
@@ -8,4 +8,13 @@ def signup(request):
 	return render(request, "core/signup.html")
 
 def signin(request):
+	if request.method == "POST":
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=email, password=password)
+		if user is not None:
+			login(request, user)
+		return redirect("/admin/")
+
 	return render(request, "core/login.html")
